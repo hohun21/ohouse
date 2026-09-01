495e82ca35063ec6c67f1dac99311be213bbc280
JMSPC\a0107 <a01077355716@gmail.com>
2026-09-01 19:16:45 +0900

    쇼핑-단독 상품 추가 + 메인 헤더 수정

---
+++ main/java/com/ohouse/shopping/only/handler/OnlyHandler.java
@@ -0,0 +1,29 @@
+package com.ohouse.shopping.only.handler;
+
+import java.util.List;
+
+import com.ohouse.common.handler.CommandHandler;
+import com.ohouse.shopping.only.dto.OnlyDTO;
+import com.ohouse.shopping.only.service.OnlyService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class OnlyHandler implements CommandHandler {
+	
+	private OnlyService onlyService = new OnlyService();
+	
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		System.out.println("OnlyHandler.process() 실행 ");
+		
+		List<OnlyDTO> products =
+				onlyService.getOnlyProducts();
+
+		request.setAttribute("products", products);
+		
+		return "/WEB-INF/views/shopping/only/only.jsp";
+	}
+
+}
